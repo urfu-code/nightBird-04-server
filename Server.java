43bc33d8bdfacfb520352435e1a670f7ebbdba9e
Anna <annagudkova94@gmail.com>
2014-04-21 21:31:23 +0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -2,6 +2,7 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
@@ -9,19 +10,22 @@ import java.net.Socket;
 
 public class Server {
 
+	private static ObjectOutputStream out;
+	private static ObjectInputStream in;
+	private static ServerSocket serverSocket;
+	private static Socket socket;
+
 	public static void main(String[] Args) throws IOException, CodeException, ClassNotFoundException {
 
-		File file = new File("world.txt");
-		MyWoodLoader loader = new MyWoodLoader();
-		PrintableWood wood = (PrintableWood) loader.Load(new FileInputStream(file));
-		ObjectOutputStream out = null;
-		ObjectInputStream in = null;
-		ServerSocket serverSocket = null;
-		Socket socket = null;
+		File file=new File("world.txt");
+		InputStream instream=new FileInputStream(file);
+		PrintableWoodLoader W=new PrintableWoodLoader();
+		PrintableWood wood=W.PrintableWoodLoad(instream,System.out);
 		Request m_request;
 		Response m_response;	
 
 		try {
+			System.out.println("Server started");
 			serverSocket = new ServerSocket(12345);
 			socket = serverSocket.accept();
 			in = new ObjectInputStream(socket.getInputStream());
@@ -47,16 +51,17 @@ public class Server {
 
 				case "CreateWoodman" : {
 					wood.createWoodman(m_request.GetName(), m_request.GetStart(), m_request.GetFinish());
+					break;
 				}
 
 				case "MoveWoodman" : {
 					currentAction = wood.move(m_request.GetName(), m_request.GetDirection());
+					m_response = new Response(currentAction);
+					out.writeObject(m_response);
+					out.flush();
+					break;
 				}
-				}
-
-				m_response = new Response(currentAction);
-				out.writeObject(m_response);
-				out.flush();
+				}		
 			}
 			System.out.println(currentAction.toString());
 		}
