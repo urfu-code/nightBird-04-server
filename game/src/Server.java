897b03b7a9812d9299b063174d5a80169e3f1caa
Anna <annagudkova94@gmail.com>
2014-04-21 21:29:04 +0600

    Update Server.java

--- game/src/Server.java
+++ game/src/Server.java
@@ -2,28 +2,30 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.InputStream;
 
 public class Server {
 
+	private static ObjectOutputStream out;
+	private static ObjectInputStream in;
+	private static ServerSocket serverSocket;
+	private static Socket socket;
+
 	public static void main(String[] Args) throws IOException, CodeException, ClassNotFoundException {
 
-		File file = new File("world.txt");
+		File file=new File("world.txt");
 		InputStream instream=new FileInputStream(file);
- 		PrintableWoodLoader W=new PrintableWoodLoader();
+		PrintableWoodLoader W=new PrintableWoodLoader();
 		PrintableWood wood=W.PrintableWoodLoad(instream,System.out);
-		ObjectOutputStream out = null;
-		ObjectInputStream in = null;
-		ServerSocket serverSocket = null;
-		Socket socket = null;
 		Request m_request;
 		Response m_response;	
 
 		try {
+			System.out.println("Server started");
 			serverSocket = new ServerSocket(12345);
 			socket = serverSocket.accept();
 			in = new ObjectInputStream(socket.getInputStream());
@@ -49,16 +51,17 @@ public class Server {
 
 				case "CreateWoodman" : {
 					wood.createWoodman(m_request.GetName(), m_request.GetStart(), m_request.GetFinish());
+					break;
 				}
 
 				case "MoveWoodman" : {
 					currentAction = wood.move(m_request.GetName(), m_request.GetDirection());
+					m_response = new Response(currentAction);
+					out.writeObject(m_response);
+					out.flush();
+					break;
 				}
-				}
-
-				m_response = new Response(currentAction);
-				out.writeObject(m_response);
-				out.flush();
+				}		
 			}
 			System.out.println(currentAction.toString());
 		}
