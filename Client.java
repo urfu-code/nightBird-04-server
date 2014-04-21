4dfced8b2854e830e82173fde6dc3562a07b1d43
Anna <annagudkova94@gmail.com>
2014-04-21 21:29:38 +0600

    Update Client.java

--- Client.java
+++ Client.java
@@ -6,26 +6,29 @@ import java.net.Socket;
 
 public class Client {
 
+	private static ObjectOutputStream out;
+	private static ObjectInputStream in;
+	private static Socket socket;
+
 	public static void main(String[] Args) throws Exception {
 
-		Socket socket = null;
 		Response m_response;
 		Request m_request;
 		System.out.println("Game started");
-		ObjectOutputStream out = null;
-		ObjectInputStream in = null;
-		
+
 		try {
 			socket = new Socket("localhost", 12345);
 			out = new ObjectOutputStream(socket.getOutputStream());
 			in = new ObjectInputStream(socket.getInputStream());
 			MyMouse Minni = new MyMouse("Minni");  
-			m_request = new Request("Minni", new Point(1,1), new Point(1,5));			
+			m_request = new Request("Minni", new Point(1,1), new Point(1,3));			
 			out.writeObject(m_request);
 			out.flush();
 
-			while (true) {
-				m_response = (Response) in.readObject();
+			Action action=Action.Ok;
+			while (!(action == Action.WoodmanNotFound) && !(action == Action.Finish)) {
+				Object readObject = in.readObject();
+				m_response = (Response) readObject;
 				if (m_response.GetResponse().equals("Action")) {
 					m_request = new Request(Minni.GetName(), Minni.NextMove(m_response.GetAction()));
 					out.writeObject(m_request);
@@ -35,7 +38,8 @@ public class Client {
 					System.out.println("Game over");
 					break;
 				}
-			}		
+				break;
+			}	
 		}
 		catch (IOException e) {
 			e.printStackTrace();
