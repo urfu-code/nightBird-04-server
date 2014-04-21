c4c76bfcbcfb39c2d4643e52bb6c5547a1763f5e
Anna <annagudkova94@gmail.com>
2014-04-21 18:17:09 +0600

    Update MyMouseTest.java

--- game/src/MyMouseTest.java
+++ game/src/MyMouseTest.java
@@ -9,38 +9,70 @@ import org.junit.Test;
 
 public class MyMouseTest {
 	@Test
-	public void testNextMoveToFinish1() throws Exception {
+	public void testNextMoveToFinish() throws Exception {
 		File file=new File("world.txt");
 		InputStream instream=new FileInputStream(file);
 		PrintableWoodLoader W=new PrintableWoodLoader();
-		PrintableWood wood=W.PrintableWoodLoad(instream,System.out);
-		Action result = Action.Ok;
-		wood.createWoodman("Dorian", new Point(1, 2), new Point(1,4));
+		PrintableWood wood=W.PrintableWoodLoad(instream,System.out);	
+		wood.createWoodman("Dorian", new Point(1, 2), new Point(1,5));
 		MyMouse mouse = new MyMouse("Dorian");
-		result = wood.move("Dorian", mouse.NextMove(result));
-		if (result != Action.Finish){
-			 			Direction direction = mouse.NextMove(result);
-			 			result = wood.move("Dorian", direction);
-			 		}
+		Direction direction= mouse.NextMove(Action.Ok);
+		try {
+			wood.move("Dorian", direction);	
+			assertEquals(direction,Direction.Right);
+			assertEquals(wood.move("Dorian", direction),Action.Ok);
+			direction= mouse.NextMove(Action.Ok);
+			assertEquals(direction,Direction.Up);
+			assertEquals(wood.move("Dorian", direction),Action.Fail);
+			direction= mouse.NextMove(Action.Fail);
+			assertEquals(direction,Direction.Right);
+			assertEquals(wood.move("Dorian", direction),Action.Finish);
+		} finally {
+			instream.close();
+		}
 	}
-	
 
-	@Test
+	@Test(expected=CodeException.class)
 	public void testNextMoveNotToFinish() throws Exception {
 		File file=new File("w.txt");
 		InputStream instream=new FileInputStream(file);
 		PrintableWoodLoader W=new PrintableWoodLoader();
-		PrintableWood wood=W.PrintableWoodLoad(instream,System.out);
-		Action result = Action.Ok;
-		wood.createWoodman("Dorian", new Point(2, 7), new Point(5, 7));
+		PrintableWood wood=W.PrintableWoodLoad(instream,System.out);	
+		wood.createWoodman("Dorian", new Point(2, 7), new Point(1,4));
 		MyMouse mouse = new MyMouse("Dorian");
-		if (result != Action.Finish){
- 			Direction direction = mouse.NextMove(result);
- 			result = wood.move("Dorian", direction);
+		try {
+			Direction direction= mouse.NextMove(Action.Ok);
+			wood.move("Dorian", direction);
+			assertEquals(direction,Direction.Right);
+			assertEquals(wood.move("Dorian", direction),Action.Fail);
+			direction= mouse.NextMove(Action.Fail);
+			assertEquals(direction,Direction.Down);
+			assertEquals(wood.move("Dorian", direction),Action.Fail);
+			direction= mouse.NextMove(Action.Fail);
+			assertEquals(direction,Direction.Left);
+			assertEquals(wood.move("Dorian", direction),Action.Fail);
+			direction= mouse.NextMove(Action.Fail);
+			assertEquals(direction,Direction.Up);
+			assertEquals(wood.move("Dorian", direction),Action.Dead);
+			direction= mouse.NextMove(Action.Fail);
+			assertEquals(direction,Direction.Right);
+			assertEquals(wood.move("Dorian", direction),Action.Fail);
+			direction= mouse.NextMove(Action.Fail);
+			assertEquals(direction,Direction.Down);
+			assertEquals(wood.move("Dorian", direction),Action.Ok);
+			assertEquals(direction,Direction.Down);
+			assertEquals(wood.move("Dorian", direction),Action.Fail);
+			direction= mouse.NextMove(Action.Fail);
+			assertEquals(direction,Direction.Left);
+			assertEquals(wood.move("Dorian", direction),Action.Fail);
+			direction= mouse.NextMove(Action.Fail);
+			assertEquals(direction,Direction.Up);
+			assertEquals(wood.move("Dorian", direction),Action.Dead);
+			direction= mouse.NextMove(Action.Fail);
+			assertEquals(direction,Direction.Right);
+			assertEquals(wood.move("Dorian", direction),Action.WoodmanNotFound);
+		} finally {
+			instream.close();
+		}
 	}
- 		//	assertEquals(result,Action.Fail);
-		assertEquals(result,Action.WoodmanNotFound);
-	}
-
-
 }
