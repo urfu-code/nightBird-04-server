697d237f280ab265003f713c8f19abed4b96f9b7
Anna <annagudkova94@gmail.com>
2014-04-22 23:47:57 +0600

    Update Request.java

--- Request.java
+++ Request.java
@@ -1,8 +1,7 @@
-
 import java.io.Serializable;
 
 public class Request implements Serializable {
-
+	
 	private static final long serialVersionUID = 1L;	
 	private String method;
 	private String m_name;
